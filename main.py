e9348cd163a0fca20a46d8b2a42ea22024b303ed
karolyiar <karolyiar@gmail.com>
2015-11-03 12:59:17 +0100

    distance

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-def readData():
-    dataList = []
+import math
+
+def dist(a_dict, b_dict):
+    a = dict(a_dict)
+    b = dict(b_dict)
+    squaresum = 0.0
+    for i in range(0, max( max(a.keys()), max(b.keys()) )):
+        ai = a.get(i, 0.0)
+        bi = b.get(i, 0.0)
+        squaresum += pow(ai-bi, 2.0)
+    return math.sqrt(squaresum)
+
+
+def read_data():
+    data_list = []
     with open("data/freq.ker", "r") as f:
         lines = f.readlines()
-        print(lines[0])
         for line in lines:
-            splitLine = line.split(" ")
-            valueList = []
-            for value in splitLine[1:]:
-                splitValue = value.split(":")
-                while (len(valueList) < int(splitValue[0]) ):
-                    valueList.append(0.0)
-                valueList.append(float(splitValue[1]))
-            dataList.append( (splitLine[0], valueList) )
-        print(dataList[0])
+            line_split = line[line.find(" "):].split(" ")
+            data_dict = {}
+            for data in line_split[1:]:
+                data_split = data.split(":")
+                data_dict[ int(data_split[0]) ] = float(data_split[1])
+            data_list.append( (line[:line.find(" ")], data_dict) )
+    print(data_list[0])
+    return data_list
+
 
 def main():
-    readData()
+    data_list = read_data()
+    distance = dist(data_list[0][1], data_list[1][1])
+    print(distance)
 
 if __name__ == "__main__":
     # execute only if run as a script
