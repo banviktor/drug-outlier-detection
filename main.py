4feef98097e2e5939888adde5d3f6f4f67f93846
Viktor Bán <viktor.ban@gmail.com>
2015-11-03 18:25:49 +0100

    More niceness.

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ def dist(data_list, a_index, b_index):
     a = dict(data_list[a_index][1])
     b = dict(data_list[b_index][1])
     square_sum = 0.0
-    for i in range(0, max( max(a.keys()), max(b.keys()) )):
+    for i in range(0, max(max(a.keys()), max(b.keys()))):
         ai = a.get(i, 0.0)
         bi = b.get(i, 0.0)
-        square_sum += pow(ai-bi, 2.0)
+        square_sum += pow(ai - bi, 2.0)
     return math.sqrt(square_sum)
 
 
 def n_k(data_list, k, a_index):
-    distances = ( dist(data_list, a_index, i) for i in range(len(data_list)) )
-    a = [ m[1] for m in (sorted((e,i) for i,e in enumerate(distances))) ]
-    return a[1:k+1]
+    distances = (dist(data_list, a_index, i) for i in range(len(data_list)))
+    a = [m[1] for m in (sorted((e, i) for i, e in enumerate(distances)))]
+    return a[1:k + 1]
 
 
 def k_distance(data_list, k, a_index):
@@ -44,6 +44,7 @@ def read_data():
 def main():
     data_list = read_data()
     print(reachability_distance(data_list, 3, 0, 2))
+
 
 if __name__ == "__main__":
     # execute only if run as a script
