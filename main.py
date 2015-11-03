55e5bd45c2bf8f2af33963ec57e573d4ac90130b
karolyiar <karolyiar@gmail.com>
2015-11-03 12:03:20 +0100

    readData

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+def readData():
+    dataList = []
+    with open("data/freq.ker", "r") as f:
+        lines = f.readlines()
+        print(lines[0])
+        for line in lines:
+            splitLine = line.split(" ")
+            valueList = []
+            for value in splitLine[1:]:
+                splitValue = value.split(":")
+                while (len(valueList) < int(splitValue[0]) ):
+                    valueList.append(0.0)
+                valueList.append(float(splitValue[1]))
+            dataList.append( (splitLine[0], valueList) )
+        print(dataList[0])
+
 def main():
-    print("a")
+    readData()
 
 if __name__ == "__main__":
+    # execute only if run as a script
     main()
