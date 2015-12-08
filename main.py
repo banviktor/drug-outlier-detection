d8b4e1a52a3707bc0ff60f44031493e353920904
karolyiar <karolyiar@gmail.com>
2015-12-08 02:11:53 +0100

    distance using numpy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+import numexpr as ne
 
 d = []
 
@@ -16,22 +18,19 @@ class Distances:
         for i, a_data in enumerate(self.data_list):
             for j, b_data in enumerate(self.data_list):
                 if a_data == b_data:
-                    continue
-                if len(a_data[1]) < len(b_data[1]):
-                    a, b = list(a_data[1]), list(b_data[1])
-                else:
-                    b, a = list(a_data[1]), list(b_data[1])
-                square_sum = 0.0
-                for k in range(len(a)):
-                    ak = a[k]
-                    bk = b[k]
-                    square_sum += pow(ak - bk, 2.0)
-                for k in range(len(a), len(b)):
-                    bk = b[k]
-                    square_sum += pow(bk, 2.0)
-                self.dist_list[i][j] = math.sqrt(square_sum)
+                    break
+                a = a_data[1]
+                b = b_data[1]
+
+                '''
+                Faster solution
+                http://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
+                '''
+                diff = np.linalg.norm(a-b)
+                self.dist_list[i][j] = diff
 
     def dist(self, a_index, b_index):
+        a_index, b_index = sorted((a_index, b_index))
         return self.dist_list[a_index][b_index]
 
 
@@ -66,30 +65,39 @@ def read_data(source):
         # count matrix columns
         cols = 0
         for l in lines:
-            cols = max(cols, l.count(":"))
-
-        # read values to a matrix
-        print(data_list)
-        for i, line in enumerate(lines):
-            split_line = line.split(" ")
-            value_list = []*cols
+            split_line = l.split(" ")
             for value in split_line[1:]:
                 split_value = value.split(":")
-                while len(value_list) < int(split_value[0]):
-                    value_list.append(0.0)
-                value_list.append(float(split_value[1]))
+                cols = max(cols, int(split_value[0])+1)
+
+        # read values to a matrix
+        for i, line in enumerate(lines):
+            split_line = line.split(" ")
+            value_list = np.zeros(cols)
+            for value in split_line[1:]:
+                split_value = value.split(":")
+                value_list[int(split_value[0])] = float(split_value[1])
             data_list.append((split_line[0], value_list))
     return data_list
 
 
 def main():
+    import time
+    start = time.time()
+
     data_list = read_data("freq.ker")
     print(data_list[0])
     global d
     d = Distances(data_list)
-    # slow
-    #print(lrd(data_list, 3, 0))
+    print(d.dist_list[0])
+    end = time.time()
+    print("Tavolsagmatrix felepitese: " + str(end - start))
 
+    # slow
+    start = time.time()
+    print(lrd(data_list, 3, 0))
+    end = time.time()
+    print("lrd kiszamitasa: " + str(end - start))
 
 if __name__ == "__main__":
     # execute only if run as a script
