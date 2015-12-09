4d1cdc5d2417a32e3ddc029c8825674fdcbf2a79
karolyiar <karolyiar@gmail.com>
2015-12-09 11:18:51 +0100

    calculating lof

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import math
 import numpy as np
 import numexpr as ne
+import functools as ft
 
-d = []
+distances = None
+kdistances = None
 
 
 class Distances:
@@ -34,27 +36,42 @@ class Distances:
         return self.dist_list[a_index][b_index]
 
 
-def n_k(data_list, k, a_index):
-    distances = (d.dist(a_index, i) for i in range(len(data_list)))
-    a = [m[1] for m in (sorted((e, i) for i, e in enumerate(distances)))]
+def n_k(data_list, k, a_index): # a
+    dist = (distances.dist(a_index, i) for i in range(len(data_list)))
+    a = [m[1] for m in (sorted((e, i) for i, e in enumerate(dist)))]
     # can be > k
     return a[1:k + 1]
 
 
-def k_distance(data_list, k, a_index):
-    return d.dist(a_index, n_k(data_list, k, a_index)[-1])
+class KDistances:
+    kd_dict = {}
+
+    def k_distance(self, data_list, k, a_index): # b = a
+        if k not in self.kd_dict:
+            self.kd_dict[k] = []
+            for i, e in enumerate(data_list):
+                self.kd_dict[k].append(distances.dist(i, n_k(data_list, k, i)[-1]))
+        return self.kd_dict[k][a_index]
 
 
-def reachability_distance(data_list, k, a_index, b_index):
-    return max(k_distance(data_list, k, b_index), d.dist(a_index, b_index))
+def reachability_distance(data_list, k, a_index, b_index): # c = n*b = n*a
+    return max(kdistances.k_distance(data_list, k, b_index), distances.dist(a_index, b_index))
 
 
-def lrd(data_list, k, a_index):
+def lrd(data_list, k, a_index): # d = n*c = n*n*b = n*n*a
     rd_sum = 0.0
     for i in range(0, len(data_list)):
         if i != a_index:
             rd_sum += reachability_distance(data_list, k, a_index, i)
     return len(n_k(data_list, k, a_index)) / rd_sum
+
+
+def lof(data_list, k, a_index): # e = n*d = n*n*n*b
+    lof_sum = 0.0
+    for b_index in range(len(data_list)):
+        if a_index != b_index:
+            lof_sum += lrd(data_list, k, b_index)
+    return lof_sum / len(n_k(data_list, k, a_index)) / lrd(data_list, k, a_index)
 
 
 def read_data(source):
@@ -87,17 +104,23 @@ def main():
 
     data_list = read_data("freq.ker")
     print(data_list[0])
-    global d
-    d = Distances(data_list)
-    print(d.dist_list[0])
+    global distances
+    distances = Distances(data_list)
+    global kdistances
+    kdistances = KDistances()
+    print(distances.dist_list[0])
     end = time.time()
     print("Tavolsagmatrix felepitese: " + str(end - start))
 
-    # slow
     start = time.time()
     print(lrd(data_list, 3, 0))
     end = time.time()
     print("lrd kiszamitasa: " + str(end - start))
+
+    start = time.time()
+    print(lof(data_list, 3, 0))
+    end = time.time()
+    print("LOF kiszamitasa: " + str(end - start))
 
 if __name__ == "__main__":
     # execute only if run as a script
