9dedbc0ec027031119304d52fec4503bb78b596b
karolyiar <karolyiar@gmail.com>
2015-12-11 13:38:27 +0100

    more optimization refactoring

--- main.py
+++ main.py
@@ -3,124 +3,151 @@ import numpy as np
 import numexpr as ne
 import functools as ft
 
-distances = None
-kdistances = None
 
+class CalculateLof:
 
-class Distances:
-    data_list = []
-    dist_list = []
+    distances = None
+    lofs = []
+    k = 0
 
-    def __init__(self, data_list):
-        self.data_list = data_list
-        self.dist_list = [[0]*len(data_list)]*len(data_list)
-        self.calc_distances()
+    class Distances:
+        data_list = []
+        dist_list = []
 
-    def calc_distances(self):
-        for i, a_data in enumerate(self.data_list):
-            for j, b_data in enumerate(self.data_list):
-                if a_data == b_data:
-                    break
-                a = a_data[1]
-                b = b_data[1]
+        def __init__(self, data_list):
+            self.data_list = data_list
+            self.dist_list = [[0]*len(data_list)]*len(data_list)
+            self.calc_distances()
 
-                '''
-                Faster solution
-                http://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
-                '''
-                diff = np.linalg.norm(a-b)
-                self.dist_list[i][j] = diff
+        def calc_distances(self):
+            for i, a_data in enumerate(self.data_list):
+                for j, b_data in enumerate(self.data_list):
+                    if a_data == b_data:
+                        break
+                    a = a_data[1]
+                    b = b_data[1]
 
-    def dist(self, a_index, b_index):
-        a_index, b_index = sorted((a_index, b_index))
-        return self.dist_list[a_index][b_index]
+                    '''
+                    Faster solution
+                    http://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
+                    '''
+                    diff = np.linalg.norm(a-b)
+                    self.dist_list[i][j] = diff
 
+        def dist(self, a_index, b_index):
+            a_index, b_index = sorted((a_index, b_index))
+            return self.dist_list[a_index][b_index]
 
-def n_k(data_list, k, a_index): # a
-    dist = (distances.dist(a_index, i) for i in range(len(data_list)))
-    a = [m[1] for m in (sorted((e, i) for i, e in enumerate(dist)))]
-    # can be > k
-    return a[1:k + 1]
+    def n_k(self, a_index):
+        data_list = self.distances.data_list
+        k = self.k
+        dist = (self.distances.dist(a_index, i) for i in range(len(data_list)))
+        a = [m[1] for m in (sorted((e, i) for i, e in enumerate(dist)))]
+        # can be > k
+        return a[1:k + 1]
 
+    '''
+    class KDistances:
+        kd_dict = {}
 
-class KDistances:
-    kd_dict = {}
+        def k_distance(self, calculate_lof, k, a_index):
+            data_list = calculate_lof.distances.data_list
+            if k not in self.kd_dict:
+                self.kd_dict[k] = []
+                for i, e in enumerate(data_list):
+                    self.kd_dict[k].append(calculate_lof.distances.dist(i, calculate_lof.n_k(i)[-1]))
+            return self.kd_dict[k][a_index]
+    '''
+    @ft.lru_cache(maxsize=1852)
+    def k_distance(self, a_index):
+        return self.distances.dist(a_index, self.n_k(a_index)[-1])
 
-    def k_distance(self, data_list, k, a_index): # b = a
-        if k not in self.kd_dict:
-            self.kd_dict[k] = []
-            for i, e in enumerate(data_list):
-                self.kd_dict[k].append(distances.dist(i, n_k(data_list, k, i)[-1]))
-        return self.kd_dict[k][a_index]
+    def reachability_distance(self, a_index, b_index):
+        k = self.k
+        return max(self.k_distance(b_index), self.distances.dist(a_index, b_index))
 
+    @ft.lru_cache(maxsize=1852)
+    def lrd(self, a_index):
+        data_list = self.distances.data_list
+        rd_sum = 0.0
+        for i in range(0, len(data_list)):
+            if i != a_index:
+                rd_sum += self.reachability_distance(a_index, i)
+        return len(self.n_k(a_index)) / rd_sum
 
-def reachability_distance(data_list, k, a_index, b_index): # c = n*b = n*a
-    return max(kdistances.k_distance(data_list, k, b_index), distances.dist(a_index, b_index))
+    def lof(self, a_index):
+        data_list = self.distances.data_list
+        lof_sum = 0.0
+        for b_index in range(len(data_list)):
+            if a_index != b_index:
+                lof_sum += self.lrd(b_index)
+        return lof_sum / len(self.n_k(a_index)) / self.lrd(a_index)
 
+    def calc_lof(self):
+        for i, e in enumerate(self.distances.data_list):
+            self.lofs.append(self.lof(i))
 
-def lrd(data_list, k, a_index): # d = n*c = n*n*b = n*n*a
-    rd_sum = 0.0
-    for i in range(0, len(data_list)):
-        if i != a_index:
-            rd_sum += reachability_distance(data_list, k, a_index, i)
-    return len(n_k(data_list, k, a_index)) / rd_sum
+    @staticmethod
+    def read_data(source):
+        data_list = []
+        with open("data/" + source, "r") as f:
+            lines = f.readlines()
 
+            # count matrix columns
+            cols = 0
+            for l in lines:
+                split_line = l.split(" ")
+                for value in split_line[1:]:
+                    split_value = value.split(":")
+                    cols = max(cols, int(split_value[0])+1)
 
-def lof(data_list, k, a_index): # e = n*d = n*n*n*b
-    lof_sum = 0.0
-    for b_index in range(len(data_list)):
-        if a_index != b_index:
-            lof_sum += lrd(data_list, k, b_index)
-    return lof_sum / len(n_k(data_list, k, a_index)) / lrd(data_list, k, a_index)
+            # read values to a matrix
+            for i, line in enumerate(lines):
+                split_line = line.split(" ")
+                value_list = np.zeros(cols)
+                for value in split_line[1:]:
+                    split_value = value.split(":")
+                    value_list[int(split_value[0])] = float(split_value[1])
+                data_list.append((split_line[0], value_list))
+        return data_list
 
+    def __init__(self, filename, k):
+        self.k = k
+        import time
+        start = time.time()
 
-def read_data(source):
-    data_list = []
-    with open("data/" + source, "r") as f:
-        lines = f.readlines()
+        data_list = CalculateLof.read_data(filename)
+        print(data_list[0])
+        self.distances = CalculateLof.Distances(data_list)
+        print(self.distances.dist_list[0])
+        end = time.time()
+        print("Tavolsagmatrix felepitese: " + str(end - start))
 
-        # count matrix columns
-        cols = 0
-        for l in lines:
-            split_line = l.split(" ")
-            for value in split_line[1:]:
-                split_value = value.split(":")
-                cols = max(cols, int(split_value[0])+1)
+        '''
+        start = time.time()
+        print(self.lrd(0))
+        end = time.time()
+        print("lrd kiszamitasa: " + str(end - start))
+        '''
 
-        # read values to a matrix
-        for i, line in enumerate(lines):
-            split_line = line.split(" ")
-            value_list = np.zeros(cols)
-            for value in split_line[1:]:
-                split_value = value.split(":")
-                value_list[int(split_value[0])] = float(split_value[1])
-            data_list.append((split_line[0], value_list))
-    return data_list
+        '''
+        start = time.time()
+        print(self.lof(0))
+        end = time.time()
+        print("LOF kiszamitasa: " + str(end - start))
+        '''
+
+        start = time.time()
+        self.calc_lof()
+        print(self.lofs[0])
+        end = time.time()
+        print("Osszes LOF kiszamitasa: " + str(end - start))
+
+        print(self.lofs[:10])
 
 
 def main():
-    import time
-    start = time.time()
-
-    data_list = read_data("freq.ker")
-    print(data_list[0])
-    global distances
-    distances = Distances(data_list)
-    global kdistances
-    kdistances = KDistances()
-    print(distances.dist_list[0])
-    end = time.time()
-    print("Tavolsagmatrix felepitese: " + str(end - start))
-
-    start = time.time()
-    print(lrd(data_list, 3, 0))
-    end = time.time()
-    print("lrd kiszamitasa: " + str(end - start))
-
-    start = time.time()
-    print(lof(data_list, 3, 0))
-    end = time.time()
-    print("LOF kiszamitasa: " + str(end - start))
+    calculateLof = CalculateLof("maccs.ker", 3)
 
 if __name__ == "__main__":
     # execute only if run as a script
